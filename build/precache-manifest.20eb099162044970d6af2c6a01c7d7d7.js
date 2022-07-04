c0e79855012c68f22f269fcd1fe56a7632acde19
Giga_sat <90619014+GigaSatx@users.noreply.github.com>
2022-07-04 21:47:45 +0530

    Add files via upload

---
+++ build/precache-manifest.20eb099162044970d6af2c6a01c7d7d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03f4b9a34f3fb4322cbff1080fd0d6dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7c5bcaf9ddd9034ecf4",
+    "url": "/static/css/2.292c8731.chunk.css"
+  },
+  {
+    "revision": "8ec53b3934be536e5597",
+    "url": "/static/css/main.1278e964.chunk.css"
+  },
+  {
+    "revision": "f7c5bcaf9ddd9034ecf4",
+    "url": "/static/js/2.a640eb33.chunk.js"
+  },
+  {
+    "revision": "8ec53b3934be536e5597",
+    "url": "/static/js/main.9f5b904d.chunk.js"
+  },
+  {
+    "revision": "ba2e3c3c81e60bb4f379",
+    "url": "/static/js/runtime-main.943706ff.js"
+  },
+  {
+    "revision": "0601b643953ad19eb771894062396db6",
+    "url": "/static/media/bg.0601b643.svg"
+  }
+]);
